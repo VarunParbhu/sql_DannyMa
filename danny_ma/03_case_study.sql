dfdcaed720fb31e10dd9c143fad533e48b4afba9
VarunParbhu <90166410+VarunParbhu@users.noreply.github.com>
2023-11-19 20:15:59 +0400

    case study 3 section b

--- danny_ma/03_case_study.sql
+++ danny_ma/03_case_study.sql
@@ -7,12 +7,127 @@ USE foodie_fi;
 -- B. Data Analysis Questions
 
 -- How many customers has Foodie-Fi ever had?
+SELECT DISTINCT TOP(1) customer_id
+FROM subscriptions
+ORDER BY customer_id DESC;
+
+
 -- What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+SELECT MONTH(start_date) AS Month,
+    COUNT(MONTH(start_date)) AS NumofSubs
+FROM subscriptions
+WHERE plan_id = 0
+GROUP BY MONTH(start_date)
+ORDER BY MONTH(start_date);
+
+
 -- What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
+SELECT plan_name,
+    COUNT(plan_name) AS NumOfCustomer
+FROM subscriptions
+    JOIN plans ON subscriptions.plan_id = plans.plan_id
+WHERE YEAR(start_date) >= 2020
+    AND YEAR(start_date) < 2021
+GROUP BY plan_name;
+
+
 -- What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
+SELECT CAST (
+        (
+            (
+                SELECT COUNT(plans.plan_name) as count
+                FROM subscriptions
+                    JOIN plans ON subscriptions.plan_id = plans.plan_id
+                WHERE plans.plan_name = 'churn'
+                GROUP BY plans.plan_name
+            ) /(
+                (
+                    SELECT CAST (
+                            (
+                                (
+                                    SELECT DISTINCT TOP(1) customer_id
+                                    FROM subscriptions
+                                    ORDER BY customer_id DESC
+                                )
+                            ) AS decimal(18, 4)
+                        ) AS count2
+                )
+            )
+        ) AS DECIMAL(18, 1)
+    ) AS percentage;
+
+
 -- How many customers have churned straight after their initial free trial - what percentage is this rounded to the nearest whole number?
+WITH CTE AS (
+    SELECT *,
+        ROW_NUMBER() OVER (
+            PARTITION BY customer_id
+            ORDER BY start_date
+        ) AS sequence
+    FROM subscriptions
+),
+CTE2 AS (
+    SELECT COUNT (customer_id) AS churnClient
+    FROM CTE
+    WHERE [sequence] = 2
+        AND plan_id = 4
+)
+SELECT CAST(
+        churnClient / (
+            SELECT CAST (
+                    (
+                        (
+                            SELECT DISTINCT TOP(1) customer_id
+                            FROM subscriptions
+                            ORDER BY customer_id DESC
+                        )
+                    ) AS decimal(18, 4)
+                ) AS count2
+        ) * 100 AS INTEGER
+    ) AS '% Churn dir'
+FROM CTE2;
+
+
 -- What is the number and percentage of customer plans after their initial free trial?
+WITH CTE AS (
+    SELECT *,
+        ROW_NUMBER() OVER (
+            PARTITION BY customer_id
+            ORDER BY start_date
+        ) AS sequence
+    FROM subscriptions
+),
+CTE2 AS (
+    SELECT plans.plan_name
+    FROM CTE
+        JOIN plans on CTE.plan_id = plans.plan_id
+    WHERE [sequence] = 2
+)
+SELECT plan_name,
+    (
+        CAST(
+            (
+                COUNT(plan_name) /(
+                    SELECT CAST (
+                            (
+                                (
+                                    SELECT DISTINCT TOP(1) customer_id
+                                    FROM subscriptions
+                                    ORDER BY customer_id DESC
+                                )
+                            ) AS decimal(18, 4)
+                        )
+                )
+            ) * 100 AS DECIMAL(18, 2)
+        )
+    ) AS '% afte trial '
+FROM CTE2
+GROUP BY plan_name
+ORDER BY [% afte trial ] DESC;
+
+
 -- What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
+
 -- How many customers have upgraded to an annual plan in 2020?
 -- How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
 -- Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
